299ebde5bfd41397697c44d9d5b376d58d5141a8
Mikhail-Mukha <152735000+Mikhail-Mukha@users.noreply.github.com>
2024-08-03 17:19:51 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import ContactForm from "./components/ContactForm/ContactForm";
+import SearchBox from "./components/SearchBox/SearchBox";
+import ContactList from "./components/ContactList/ContactList";
+import stateApp from "./stateApp.json";
+import { useEffect, useState } from "react";
+
+const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    const savedData = JSON.parse(window.localStorage.getItem("contacts"));
+    return savedData || stateApp;
+  });
+
+  const [filter, setFilter] = useState("");
+
+  const changeFilter = (value) => {
+    setFilter(value);
+  };
+
+  const filteredContacts = contacts.filter(
+    (contact) =>
+      contact.name.toLowerCase().includes(filter.toLowerCase()) ||
+      contact.number.includes(filter)
+  );
+
+  const addContact = (contact) => {
+    setContacts((prevContacts) => [...prevContacts, contact]);
+  };
+
+  const deleteContact = (id) => {
+    setContacts((prevContacts) =>
+      prevContacts.filter((contact) => contact.id !== id)
+    );
+  };
+
+  useEffect(() => {
+    window.localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm addContact={addContact} />
+      <SearchBox filter={filter} changeFilter={changeFilter} />
+      <ContactList contacts={filteredContacts} deleteContact={deleteContact} />
+    </div>
+  );
+};
+
+export default App;
